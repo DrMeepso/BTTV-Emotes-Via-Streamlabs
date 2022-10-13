5db98bee83661062a4fdc6d62dbf7130a69b0926
Meep <50252724+DrMeepso@users.noreply.github.com>
2022-10-14 11:17:03 +1300

    Update loader.js

--- loader.js
+++ loader.js
@@ -11,6 +11,6 @@ var Script = document.createElement("script")
 fetch("https://raw.githubusercontent.com/DrMeepso/BTTV-Emotes-Via-Streamlabs/main/index.js")
 	.then( data => data.text() )
 	.then( js => {
-		Script.innerHTML = js.replace("<TwitchID>", TwitchID)
+		Script.innerHTML = js.replace(`"<TwitchID>"`, TwitchID)
   	document.body.append(Script)
 	})
