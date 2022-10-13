5532d9f91fce569cf3a309936f8d50b271075cf8
Meep <50252724+DrMeepso@users.noreply.github.com>
2022-10-14 11:04:03 +1300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,12 @@
+/*
+	This isnt the whole script. 
+	This just loads the latest vertion of the script so any fixes that get added will automaticly apply.
+	
+	The actual script can be found under the name Index.js!
+*/
 var TwitchID = 219012725 // Your twitch channel ID that is linked to your BTTV account
 //----------------------------------------------------------------------------------//
+// Dont edit any of this code! unless you know what your doing
 var Script = document.createElement("script")
 fetch("https://raw.githubusercontent.com/DrMeepso/BTTV-Emotes-Via-Streamlabs/main/index.js")
 	.then( data => data.text() )
