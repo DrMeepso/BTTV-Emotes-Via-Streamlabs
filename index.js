9630ade3356c2908f8736a32bd124fa99175f611
Meep <50252724+DrMeepso@users.noreply.github.com>
2022-10-14 07:55:41 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ fetch(`https://api.betterttv.net/3/cached/users/twitch/${UserID}`)
 	.then((response) => response.json())
 	.then((data) => {
   	data.channelEmotes.forEach(e => {
-      ChannelEmotes.push(e)
-    })
-    data.sharedEmotes.forEach(e => {
-      ChannelEmotes.push(e)
-    })
-	});
+      		ChannelEmotes.push(e)
+    	})
+    	data.sharedEmotes.forEach(e => {
+      		ChannelEmotes.push(e)
+    	})
+});
 
 fetch(`https://api.betterttv.net/3/cached/emotes/global`)
 	.then((response) => response.json())
@@ -18,15 +18,15 @@ fetch(`https://api.betterttv.net/3/cached/emotes/global`)
   	data.forEach(e => {
       ChannelEmotes.push(e)
     })
-	});
+});
 
 document.addEventListener('onEventReceived', function(obj) {
-  	var messages = Object.values(document.getElementsByClassName("message"))
+  var messages = Object.values(document.getElementsByClassName("message"))
     messages.forEach(object => {
+      //object.setAttribute("BTTVUpdate", true)
       ChannelEmotes.forEach(emote => {
         object.innerHTML = object.innerHTML.replaceAll(emote.code, `<span class="emote" style="background-image: url(https://cdn.betterttv.net/emote/${emote.id}/3x);"><img src="https://cdn.betterttv.net/emote/${emote.id}/3x"></span>`)
+      	object.className = "messageBTTV"
       })
-    })
-    
-    
+    })    
 });
